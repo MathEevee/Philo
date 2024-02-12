764f9b6b4de2eb6a922ab10f470b98505f76333e
Mathilde De Oliveira <matde-ol@2A6.42angouleme.fr>
2024-02-12 22:29:24 +0100

    test

--- src/simulation.c
+++ src/simulation.c
@@ -6,7 +6,7 @@
 /*   By: matde-ol <matde-ol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:34:13 by matde-ol          #+#    #+#             */
-/*   Updated: 2024/02/12 15:42:49 by matde-ol         ###   ########.fr       */
+/*   Updated: 2024/02/12 22:29:01 by matde-ol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 void	philo_init(t_data *data, t_philo **philo)
 {
+	int	i;
+
+	i = 0;
 	philo = malloc(sizeof(t_philo) * (data->nbr_of_philo + 1));
 	if (philo == NULL)
-		return (-1);
+		return ;
+	while (i < data->nbr_of_philo)
+	{
+		philo[i]->forkr = pthread_mutex_init()
+		i++;
+	}
 }
